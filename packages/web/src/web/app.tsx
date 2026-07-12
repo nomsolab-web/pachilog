19bdab3fcb29f1409cc906ce1dcf902619aa3068
nomsolab-web <nomso.lab@gmail.com>
2026-07-13 00:57:33 +0900

    feat: wire up weekly summary routes in app (retry)

--- packages/web/src/web/app.tsx
+++ packages/web/src/web/app.tsx
@@ -3,6 +3,8 @@ import Index from "./pages/index";
 import ChannelPage from "./pages/channel";
 import MachinesPage from "./pages/machines";
 import MachinePage from "./pages/machine";
+import WeeklyPage from "./pages/weekly";
+import WeeklyDetailPage from "./pages/weekly-detail";
 import { Layout } from "./components/layout";
 import { Provider } from "./components/provider";
 import { AgentFeedback, RunableBadge } from "@runablehq/website-runtime";
@@ -16,6 +18,8 @@ function App() {
           <Route path="/channels/:id" component={ChannelPage} />
           <Route path="/machines" component={MachinesPage} />
           <Route path="/machines/:id" component={MachinePage} />
+          <Route path="/weekly" component={WeeklyPage} />
+          <Route path="/weekly/:weekOf" component={WeeklyDetailPage} />
         </Switch>
       </Layout>
       {import.meta.env.DEV && <AgentFeedback />}
